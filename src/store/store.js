f1c58b8257eab75455c412879d513b472fbec3c2
thongnd <thongnd@d-soft.com.vn>
2025-06-09 17:19:07 +0700

    office/update

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import videoSlide from "./videoSlide";
+import matchSlide from "./matchSlide";
 
 const store = configureStore({
   reducer: {
     video: videoSlide,
+    match: matchSlide,
   },
 });
 
